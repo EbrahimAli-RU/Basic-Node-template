9567a05401c4ab06d9a13632c6d312ebcd63bcb6
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-01-15 02:23:35 +0600

    Handle duplicate value error in globally

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,5 @@
+const appError = require('../utils/appError')
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -10,7 +12,7 @@ const sendErrorDev = (err, res) => {
 const sendErrorProd = (err, res) => {
     if (err.isOperational) {
         res.status(err.statusCode).json({
-            status,
+            status: err.status,
             message: err.message,
         })
     } else {
@@ -21,18 +23,26 @@ const sendErrorProd = (err, res) => {
     }
 }
 
+const handleMongoDuplicateKeyError = (err) => {
+    let dubKeyValuePair = Object.entries(err.keyValue)[0]
+    let message = `Duplicate value ${dubKeyValuePair[0]}: ${dubKeyValuePair[1]}`
+
+    return new appError(message, 400)
+}
 
 
 module.exports = (err, req, res, next) => {
+
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    console.log(err.code)
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
 
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res)
+        let error = { ...err }
+        if (error.code === 11000) error = handleMongoDuplicateKeyError(error)
+        sendErrorProd(error, res)
     }
 
 
