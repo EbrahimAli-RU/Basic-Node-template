45d10841c836de71d04dce4b298d4771ba943dd2
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-01-15 22:20:39 +0600

    seperate datavalidity and validateEmail

--- controller/authController.js
+++ controller/authController.js
@@ -4,46 +4,11 @@ const User = require('../model/user')
 const catchAsync = require('../utils/catchAsync')
 const appError = require('../utils/appError')
 const sendMail = require('../utils/email')
-
-const validateEmail = email => {
-    return (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email)
-}
-const dataValidity = (userName, email, password, confirmPassword) => {
-    if (!userName || !email || !password || !confirmPassword) {
-        return {
-            message: `All fields are required`,
-            statusCode: 400
-        }
-    } else if (!validateEmail(email)) {
-        return {
-            message: `Not an email, please provide a valid one!`,
-            statusCode: 400
-        }
-    } else if (password.length < 8) {
-        return {
-            message: `Password must be at least 8 charecter`,
-            statusCode: 400
-        }
-    } else if (confirmPassword.length < 8) {
-        return {
-            message: `confirmPassword must be at least 8 charecter`,
-            statusCode: 400
-        }
-    } else if (password !== confirmPassword) {
-        return {
-            message: `Password is not equal to confirmpassword`,
-            statusCode: 400
-        }
-    } else {
-        return false
-    }
-
-}
-
+const { dataValidity, validateEmail } = require('../utils/validity')
 exports.register = catchAsync(async (req, res, next) => {
 
     const { userName, email, password, confirmPassword } = req.body
-    const result = dataValidity(userName, email, password, confirmPassword)
+    const result = dataValidity(userName.trim(), email.trim(), password.trim(), confirmPassword.trim())
     if (result) {
         return next(new appError(result.message, result.statusCode))
     }
@@ -56,7 +21,7 @@ exports.register = catchAsync(async (req, res, next) => {
         email,
         password,
         confirmPassword
-    }, process.env.PRIVATE_KEY, { expiresIn: 100 })
+    }, process.env.PRIVATE_KEY, { expiresIn: '15m' })
     const activationUrl = `${process.env.CLIENT_URL}/user/activation/${token}`
     await sendMail({
         url: activationUrl
@@ -65,24 +30,30 @@ exports.register = catchAsync(async (req, res, next) => {
         status: 'success',
         message: `Send an Email to ${email}`
     })
-
-    // next()
 })
 
 exports.activation = catchAsync(async (req, res, next) => {
-    console.log("EBRAHIM")
     const { activationToken } = req.body
-    const decoded = await jwt.verify(activationToken, process.env.PRIVATE_KEY);
-    console.log(decoded)
-    // const newUser = await User.create({
-    //     userName: decoded.userName,
-    //     email: decoded.email,
-    //     password: decoded.password,
-    //     confirmPassword: decoded.confirmPassword
-    // })
+    jwt.verify(activationToken, process.env.PRIVATE_KEY, async function (err, decoded) {
+        if (err) {
+            switch (err.name) {
+                case 'TokenExpiredError':
+                    return next(new appError(`This token is no longer valid`, 400))
+                // case ''
+            }
+        } else {
+            const newUser = await User.create({
+                userName: decoded.userName,
+                email: decoded.email,
+                password: decoded.password,
+                confirmPassword: decoded.confirmPassword
+            })
 
-    res.status(201).json({
-        status: 'success',
-        user: newUser
-    })
+            res.status(201).json({
+                status: 'success',
+                user: newUser
+            })
+
+        }
+    });
 })
