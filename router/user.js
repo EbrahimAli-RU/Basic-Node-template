6cc09d4c0369b0ca4dbe11fd6471bafca56f59a8
Ebrahim <ebrahimali.cse.ru@gmail.com>
2022-03-18 12:34:44 +0600

    fix some of bugs

--- router/user.js
+++ router/user.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const authController = require('../controller/authController')
+const authController = require("../controller/authController");
 
-router.route('/register').post(authController.register);
-router.post('/activation', authController.activation)
-router.post('/signin', authController.signIn)
-router.post('/forgotpassword', authController.forgotPassword)
-router.patch('/resetpassword', authController.resetPassword)
-router.patch('/updatepassword', authController.protected, authController.updatePassword)
-router.get('/', authController.protected, authController.getAllUsers)
+router.route("/register").post(authController.withOutEmailVerificationRegister);
+router.post("/activation", authController.activation);
+router.post("/signin", authController.signIn);
+router.post("/forgotpassword", authController.forgotPassword);
+router.patch("/resetpassword", authController.resetPassword);
+router.patch(
+  "/updatepassword",
+  authController.protected,
+  authController.updatePassword
+);
+router.get("/", authController.protected, authController.getAllUsers);
 
-module.exports = router
+module.exports = router;
